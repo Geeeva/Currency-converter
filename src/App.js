a696c2c2beaaa011f5a2373c76d988ce1897971c
Ivana <ivana.gilic@datadesign.me>
2021-01-29 13:59:34 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 import CurrencyFromRow from "./components/CurrencyFromRow";
 import CurrencyToRow from "./components/CurrencyToRow";
-import {myData} from "./data/data";
-
-
-
-
 function App() {
-
-
-
 
     const [currencyOptions, setCurrencyOptions] = useState([]);
     const [data, setData] = useState();
@@ -26,26 +18,35 @@ function App() {
     }, []);
 
     const getResults = () => {
-        fetch("./data/data.json")
+        fetch('./data.json'
+   )
             .then(res => res.json())
             .then(data => {
-                const firstCurrency = Object.keys(data.rates)[0];
-                setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
                 console.log(data);
-                console.log(data["eur"]);
+                const firstCurrency = Object.keys(data[0].rates)[0];
+                const currencyOptions = data.map(
+                    currency => (
+                        currency.base
+                    )
+                )
+                setCurrencyOptions(currencyOptions);
                 setData(data);
-                setFromCurrency(data.base);
+                setFromCurrency(data[0].base);
                 setToCurrency(firstCurrency);
-                setExchangeRate(data.rates[firstCurrency]);
+                setExchangeRate(data[0].rates[firstCurrency]);
                 setToAmount(exchangeRate);
             })}
 
     const handleSubmit = event =>{
         event.preventDefault();
-        setExchangeRate(data.rates[toCurrency]);
+        /*setExchangeRate(data[0].rates[toCurrency]);
+        console.log(data[0].rates[toCurrency]);
         setToCurrency(setToCurrency);
         let toAmount = amount * exchangeRate;
-        setToAmount(toAmount);
+        setToAmount(toAmount);*/
+        data.map(currency =>(
+            currency.filter()
+        ))
     }
 
     return (
@@ -53,18 +54,18 @@ function App() {
         <h1>Convert</h1>
         <form onSubmit={handleSubmit}>
         <input type="number" value={amount} onChange={event => setAmount(event.target.value)}/>
-    <CurrencyFromRow
-    currencyOptions={currencyOptions}
-    selectedCurrency={fromCurrency}
-    onChangeCurrency={event => {setFromCurrency(event.target.value); console.log(event.target.value);}}
-    />
-    <div>=</div>
-    <CurrencyToRow
-    currencyOptions={currencyOptions}
-    selectedCurrency={toCurrency}
-    onChangeCurrency={event => setToCurrency(event.target.value)}
-    />
-    </form>
+            <CurrencyFromRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={fromCurrency}
+            onChangeCurrency={event => {setFromCurrency(event.target.value); console.log(event.target.value);}}
+            />
+            <div>=</div>
+            <CurrencyToRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={toCurrency}
+            onChangeCurrency={event => setToCurrency(event.target.value)}
+            />
+        </form>
     <div>{toAmount}</div>
     </div>
 );
